2e1d6ae0d8705cb535f73ef5f621561efc172ec4
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-03-28 10:32:56 -0300

    refactor: change the way that verifies that the new user already exists or not

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -24,21 +24,18 @@ export const login = async (req: Request, res: Response) => {
 };
 
 export const register = async (req: Request, res: Response) => {
-
 	// Todo: Add a regEx to verify the data.
-
 	try {
-		if (!req.body.email && !req.body.password) return res.status(400).send({ status: 'Please, send all information.' });
-		const newUser = new User();
-		newUser.email = req.body.email;
-		newUser.password = req.body.password;
-		try {
-			await newUser.save();
-			console.log('Novo usuario cadastrado: ', newUser);
-			res.status(200).send({ status: 'ok', newUser });
-		} catch(error) {
-			res.status(400).send({status: 'error', information: 'User already exists.'});
-		}
+		if (!req.body.email && !req.body.password) return res.status(400).send({ status: 'Please, provide email and password.' });
+		const existingUser = await User.findOne({ email: req.body.email });
+		if (existingUser) return res.status(409).send({ status: 'error', information: 'User already exists.' });
+		const newUser = new User({
+			email: req.body.email,
+			password: req.body.password,
+		});
+		await newUser.save();
+		console.log('New user registered: ', newUser);
+		res.status(201).send({ status: 'ok', newUser });
 	} catch (error) {
 		res.status(400).send(error);
 	}
